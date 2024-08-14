0177379e8f2ee1a3f57708570b991400ad83971e
hadily <hadilyahiaoui02@gmail.com>
2024-08-14 23:16:36 +0100

    Adding Product && Event controllers

---
+++ app/src/Controller/EventController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+#[Route('/api/event')]
+class EventController extends AbstractController
+{
+    #[Route('/event', name: 'app_event')]
+    public function index(): Response
+    {
+        return $this->render('event/index.html.twig', [
+            'controller_name' => 'EventController',
+        ]);
+    }
+}
